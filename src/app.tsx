651bf49d35429211397a6fb379bf219458373b65
xielingsheng <1550464586@qq.com>
2022-02-22 23:53:22 +0800

    feat: 删除scss文件

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import style from './App.scss'
 import { Button } from 'antd'
 import 'antd/dist/antd.css'
 const App: React.FC<any> = () => {
   return (
     <>
-      <div className={style.app}>Hello World!!!</div>
+      <div>Hello World!!!</div>
       <Button type="primary">666</Button>
     </>
   )
